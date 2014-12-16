4234eea8d0a890840438649ce5b5a448d2a726ac
yongxu <renyongxu@gmail.com>
2014-12-15 18:24:41 -0600

    game playable

--- app/code/code.py
+++ app/code/code.py
@@ -1,29 +1,42 @@
 import random
+from math import *
 EMPTY=0
 WALL=1
 MONSTER=2
-PLAYER=3
-APPLE=4
-OUTSIDEMAP=-1
+PLAYER=4
+NOT_MONSTER_PLAYER=~(MONSTER+PLAYER)
+MONSTER_PLAYER=(MONSTER+PLAYER)
+APPLE=8
+OUTSIDEMAP=1024
+
+
 class Minion():
     def __init__(self,board,kind,texture,x,y):
         self.board=board
         self.map=board.map
         self.x=x
         self.y=y
+        board.removeSprite(x,y)
         self.sprite=Sprite(texture,x,y)
         self.kind=kind
-        self.map[x][y]=kind
+        self.map[x][y]=self.map[x][y] ^ kind
+
     def setPosition(self,x,y):
-        self.map[self.x][self.y]=EMPTY
+        self.map[self.x][self.y]=self.map[self.x][self.y] & (~ self.kind)
         self.x=x%21
         self.y=y%21
         self.sprite.moveTo(self.x,self.y)
-        self.map[self.x][self.y]=self.kind
+        self.map[self.x][self.y]=self.map[self.x][self.y] ^ self.kind
+
     def getPosition(self):
         return (self.x,self.y)
     def aroundInfo(self):
         return self.board.aroundInfo(self.x,self.y);
+
+    def remove(self):
+        self.map[self.x][self.y]=self.map[self.x][self.y] & (~ self.kind)
+        self.sprite.remove()
+
     def move(self,cmd):
         if cmd=='up':
             self.setPosition(self.x,self.y-1)
@@ -33,6 +46,7 @@ class Minion():
             self.setPosition(self.x-1,self.y)
         elif cmd=='right':
             self.setPosition(self.x+1,self.y)
+
     def okToMove(self,cmd,okToLand):
         if cmd=='up':
             return self.board.positionHas(self.x,self.y-1,okToLand)
@@ -45,15 +59,18 @@ class Minion():
 
 class Player(Minion):
     def __init__(self,board,texture,x,y):
+        self.score=0
         Minion.__init__(self,board,PLAYER,texture,x,y)
     def setPosition(self,x,y):
-        self.map[self.x][self.y]=EMPTY
-        self.x=x%21
-        self.y=y%21
-        self.sprite.moveTo(self.x,self.y)
-        if self.map[self.x][self.y]==APPLE:
-            self.board.removeApple(self.x,self.y)
-        self.map[self.x][self.y]=self.kind
+        x=x%21
+        y=y%21
+        if self.map[x][y] & APPLE:
+            self.score=self.score+1
+            self.board.removeApple(x,y)
+        elif self.map[x][y]==MONSTER:
+            #here,dead
+            return #dead
+        Minion.setPosition(self,x,y)
 
 
 class Board():
@@ -67,18 +84,36 @@ class Board():
                 self.map[i].append(EMPTY)
                 self.sprites[i].append(None)
         self.createWalls()
-        self.player=Player(self,"player",playerPos[0],playerPos[1])
-        self.monsters=[Minion(self,PLAYER,"draco green",monster1Pos[0],monster1Pos[1]),
-                       Minion(self,PLAYER,"draco black",monster2Pos[0],monster2Pos[1])]
         self.fillWithApples()
+        self.player=Player(self,"player",playerPos[0],playerPos[1])
+        self.monsters=[Minion(self,MONSTER,"draco green",monster1Pos[0],monster1Pos[1]),
+                       Minion(self,MONSTER,"draco black",monster2Pos[0],monster2Pos[1])]
+
+    def getMonster(self,pos):
+        for m in self.monsters:
+            if m.getPosition()==pos:
+                return m
+        return None
+
+    def mapCopy(self):
+        cp=[]
+        for i in range(0,21):
+            cp.append([])
+            for j in range(0,21):
+                cp[i].append(self.map[i][j])
+        return cp
+
+    def getSprite(self,pos):
+        return self.sprites[pos[0]][pos[1]]
+
     def positionInfo(self,x,y):
         if not (0<=x<21 and 0<=y<21):
             return OUTSIDEMAP;
         else:
             return self.map[x][y]
 
-    def positionHas(self,x,y,list):
-        return self.positionInfo(x,y) in list
+    def positionHas(self,x,y,has):
+        return (self.positionInfo(x%21,y%21) & (~has))==0
 
     def randomFind(self,target):
         x=random.randint(0,20)
@@ -95,13 +130,21 @@ class Board():
                     self.addApple(x,y)
 
     def addApple(self,x,y):
+        self.removeSprite(x,y)
         self.sprites[x][y]=Sprite('apple',x,y)
-        self.map[x][y]=APPLE
+        self.map[x][y]=self.map[x][y] ^ APPLE
 
     def removeApple(self,x,y):
-        self.sprites[x][y].remove()
-        print "removeApple!"
-        self.map[x][y]=EMPTY
+        if self.map[x][y] & APPLE:
+            if self.sprites[x][y]!=None:
+                self.sprites[x][y].remove()
+            self.map[x][y]=self.map[x][y] & (~APPLE)
+
+    def removeSprite(self,x,y):
+        if self.map[x][y] & NOT_MONSTER_PLAYER:
+            if self.sprites[x][y]!=None:
+                self.sprites[x][y].remove()
+            self.map[x][y]=self.map[x][y] & MONSTER_PLAYER
 
 
     def aroundInfo(self,x,y):
@@ -109,9 +152,11 @@ class Board():
                 'down':self.positionInfo(x,y+1),
                 'left':self.positionInfo(x-1,y),
                 'right':self.positionInfo(x+1,y)}
+
     def createWall(self,x,y):
         self.sprites[x][y]=Sprite('wall',x,y)
-        self.map[x][y]=1
+        self.map[x][y]=WALL
+
     def createWalls(self):
         for i in range(1,22):
             self.createWall(i-1,21-1)
@@ -156,7 +201,64 @@ class Board():
         for i in [1, 21]:
             self.createWall(i-1,2-1)
 
+
+def distance(a,b):
+    return abs(a[0]-b[0])+abs(a[1]-b[1])
+
+def recordMap():
+    map=[]
+    for i in range(0,21):
+        map.append([])
+        for j in range(0,21):
+            map[i].append(None)
+    return map
+
+def adjacentPos(x,y):
+    return [(x-1,y),(x+1,y),(x,y-1),(x,y+1)]
+
+# class Agent():
+#     def __init__(self,board):
+#         self.board=board
+#         self.map=board.mapCopy()
+#         self.player=board.player.getPosition()
+#         self.monsters=[m.getPosition() for m in board.monsters]
+
+#     def findClosestApples(self,obj):
+
+def getPath(record,pos,path):
+    (px,py)=record[pos[0]][pos[1]]
+    if px==pos[0] and py==pos[1]:
+        return path
+    else:
+        path.insert(0,pos)
+    return getPath(record,(px,py),path)
+def bfs(map,start,Lookingfor,n=-1):
+    found=[]
+    frontier=[start]
+    record=recordMap()
+    record[start[0]][start[1]]=start
+    while len(frontier)!=0:
+        pos=frontier.pop(0)
+        for (x,y) in adjacentPos(pos[0],pos[1]):
+
+            if (not (0<=x<21 and 0<=y<21)) or record[x][y]!=None:
+               pass 
+            elif map[x][y] & Lookingfor:
+                n=n-1
+                record[x][y]=pos
+                found.append( { 'start':start,'end':(x,y), 'path':getPath(record,(x,y),[]) } )
+                if n==0:
+                    return found
+                frontier.append((x,y))
+            elif not (map[x][y] & WALL):
+                record[x][y]=pos
+                frontier.append((x,y))
+    return found
+
 board=Board((1,1),(1,18),(3,3))
+#res=bfs(board.map,(1,1),MONSTER,2)
+#res=bfs(board.map,(1,1),APPLE,5)
+#print res
 
 def test():
  #   (x,y)=board.player.getPosition()
@@ -176,17 +278,25 @@ def test():
 
 def key(k,id):
     print k,id
-    if id=="Up":
-        if board.player.okToMove("up",[EMPTY,APPLE]):
+    if id=="Up":  
+        if board.player.okToMove("up",EMPTY^APPLE):
             board.player.move("up")
     elif id=="Down":
-        if board.player.okToMove("down",[EMPTY,APPLE]):
+        if board.player.okToMove("down",EMPTY^APPLE):
             board.player.move("down")
     elif id=="Left":
-        if board.player.okToMove("left",[EMPTY,APPLE]):
+        if board.player.okToMove("left",EMPTY^APPLE):
             board.player.move("left")
     elif id=="Right":
-        if board.player.okToMove("right",[EMPTY,APPLE]):
+        if board.player.okToMove("right",EMPTY^APPLE):
+            print "right"
             board.player.move("right")
 
+    res=bfs(board.map,board.player.getPosition(),MONSTER,2)
+    print res
+    for m in res:
+        monster=board.getMonster(m['end'])
+        if len(m['path'])==1:
+            return # dead
+        monster.setPosition(m['path'][-2][0],m['path'][-2][1])
 keydown(key)
